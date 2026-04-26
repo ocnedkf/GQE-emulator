8d95b8d89c6fa5b4231f952eb5797b3f7e7b2d1c
ocnedkf <143856368+ocnedkf@users.noreply.github.com>
2026-04-26 13:46:31 +0800

    Simplify environment variable exports for wine64

--- start-gqe.sh
+++ start-gqe.sh
@@ -2,10 +2,4 @@
 pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
 termux-x11 :13
 grun --shell
-export DISPLAY=:13
-export MESA_NO_ERROR=1
-export DXVK_HUD=1
-export PULSE_SERVER=127.0.0.1
-export LOCPATH=$HOME/.locale
-export DXVK_ASYNC=1
-box64 wine64 explorer /desktop=1024x768 TFM.exe
+env DXVK_ASYNC=1 WINEPREFIX=$HOME/.gqe-data LOCPATH=$HOME/.locale PULSE_SERVER=127.0.0.1 DXVK_HUD=1 MESA_NO_ERROR=1 DISPLAY=:13 box64 wine64 explorer /desktop=1024x768 TFM.exe

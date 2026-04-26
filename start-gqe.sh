f7e3d14aad5b133907c527cc34c33fe7be93e54e
ocnedkf <143856368+ocnedkf@users.noreply.github.com>
2026-04-26 13:50:18 +0800

    Change DXVK_ASYNC to BOX64_ASYNC in start-gqe.sh

--- start-gqe.sh
+++ start-gqe.sh
@@ -2,4 +2,4 @@
 pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
 termux-x11 :13
 grun --shell
-env DXVK_ASYNC=1 WINEPREFIX=$HOME/.gqe-data LOCPATH=$HOME/.locale PULSE_SERVER=127.0.0.1 DXVK_HUD=1 MESA_NO_ERROR=1 DISPLAY=:13 box64 wine64 explorer /desktop=1024x768 TFM.exe
+env BOX64_ASYNC=1 WINEPREFIX=$HOME/.gqe-data LOCPATH=$HOME/.locale PULSE_SERVER=127.0.0.1 DXVK_HUD=1 MESA_NO_ERROR=1 DISPLAY=:13 box64 wine64 explorer /desktop=1024x768 TFM.exe
